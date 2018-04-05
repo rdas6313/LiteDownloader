8445e98ee87481023ea82c91904226266730b4ac
Raja <rdas6313@gmail.com>
2018-04-05 11:44:30 +0530

    Completed

--- app/src/main/java/com/example/rdas6313/litedownloader/MainActivity.java
+++ app/src/main/java/com/example/rdas6313/litedownloader/MainActivity.java
@@ -16,55 +16,12 @@ import com.example.litedownloaderapi.TaskManager;
 
 import java.io.File;
 
-public class MainActivity extends AppCompatActivity implements LiteDownloadListener {
+public class MainActivity extends AppCompatActivity{
 
-    private TextView fnameView,statusView;
-    private ProgressBar progressBarView;
-    private LiteDownloader liteDownloader;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        statusView = (TextView)findViewById(R.id.status);
-        fnameView = (TextView)findViewById(R.id.filename);
-        progressBarView = (ProgressBar)findViewById(R.id.progressBar);
-        String path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getAbsolutePath();
-        String url = "http://media.djmazadownload.xyz/music/Singles/Ghar%20Se%20Nikalte%20Hi%20-%20DJMaza.Fun%20-190Kbps.mp3";
-        String name = "abc";
-        statusView.setText("No Status");
-        Request request = DownloadRequest.getRequest();
-        request.setDownloadUrl(url).setDir(path).setFileName(name);
-        liteDownloader = TaskManager.getManager();
-        liteDownloader.add(request);
-        liteDownloader.setCallbackListener(this);
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        liteDownloader.clear();
-    }
-
-    @Override
-    public void onStart(int id) {
-        statusView.setText("Download Started");
-    }
-
-    @Override
-    public void onError(int id, String errorMsg, int errorCode) {
-        statusView.setText("Downlaod Error");
-        Log.e("MainActivity",errorCode+" "+errorMsg);
-    }
-
-    @Override
-    public void onProgress(int id, long fileSize, long downloadedSize, int progress) {
-        progressBarView.setProgress(progress);
-        statusView.setText("Download Progressing");
-    }
-
-    @Override
-    public void onSuccess(int id) {
-        progressBarView.setProgress(100);
-        statusView.setText("Download Success");
-    }
 }
